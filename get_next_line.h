e53211e03e5dfc56fbb40b745a1147cadd6f9457
chan-sen <chansen1532@hotmail.com>
2017-02-07 16:31:48 -0800

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -5,18 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: chansen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/09 18:55:05 by chansen           #+#    #+#             */
-/*   Updated: 2016/11/09 18:57:41 by chansen          ###   ########.fr       */
+/*   Created: 2016/11/28 17:59:00 by chansen           #+#    #+#             */
+/*   Updated: 2016/11/28 17:59:03 by chansen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define	BUFF_SIZE	14
 # include "./libft/libft.h"
 # include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+# define BUFF_SIZE	42
+
+# define MAX_FD		2147483647
 
 int		get_next_line(const int fd, char **line);
- 
+
 #endif
